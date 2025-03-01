48496e3acecff388118187e101a175ea693047be
Keropunchh <kritsakonp601@gmail.com>
2025-03-01 17:13:37 +0700

    Merge Routes

--- server/routes/allRoutes.js
+++ server/routes/allRoutes.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const teamRoutes = require("./teamRoutes");
+const matchRoutes = require("./matchRoutes");
 
 // รวมทุก routes
 router.use("/teams", teamRoutes);
+router.use("/matches", matchRoutes);
 
 module.exports = router;
